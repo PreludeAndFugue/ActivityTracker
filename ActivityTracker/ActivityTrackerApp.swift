2a9f68750ef091e6abaf40c89c32606dac02786b
Gary Kerr <garykerrwork@gmail.com>
2021-06-23 00:05:27 +0100

    Initial Commit

---
+++ ActivityTracker/ActivityTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ActivityTrackerApp.swift
+//  ActivityTracker
+//
+//  Created by gary on 23/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct ActivityTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
