7b3ce4136d263e759f5d80e3c3e2dd11e0839800
Gary Kerr <garykerrwork@gmail.com>
2021-07-04 22:28:16 +0100

    Add linear gradient to elevation profile

--- ActivityTracker/Views/ActivityDetail/ElevationView/ElevationView.swift
+++ ActivityTracker/Views/ActivityDetail/ElevationView/ElevationView.swift
@@ -13,7 +13,7 @@ struct ElevationView: View {
     var body: some View {
         DisclosureGroup("Elevation profile") {
             ElevationPath(points: coordinator.activityElevationData)
-                .fill(Color.red)
+                .fill(linearGradient)
                 .frame(width: 1000, height: 150)
         }
         .padding(8)
@@ -24,22 +24,38 @@ struct ElevationView: View {
 }
 
 
+// MARK: - Private
+
+private extension ElevationView {
+    var linearGradient: LinearGradient {
+        let translucentRed = Color(.sRGB, red: 1, green: 0, blue: 0, opacity: 0.4)
+        let gradient = Gradient(colors: [.red, translucentRed])
+        return LinearGradient(
+            gradient: gradient,
+            startPoint: .zero,
+            endPoint: UnitPoint(x: 0, y: 1)
+        )
+    }
+}
+
+
 private struct ElevationPath: Shape {
     let points: [DistanceElevation]
 
     func path(in rect: CGRect) -> Path {
         var p = Path()
-        p.move(to: .zero)
+        p.move(to: CGPoint(x: 0, y: 120))
         for point in points {
-            p.addLine(to: CGPoint(x: point.distance, y: point.elevation))
+            p.addLine(to: CGPoint(x: point.distance, y: 120 - point.elevation))
         }
         let x = p.currentPoint!.x
-        p.addLine(to: CGPoint(x: x, y: 0))
+        p.addLine(to: CGPoint(x: x, y: 120))
         p.closeSubpath()
         let b = p.boundingRect
-        let t = CGAffineTransform(scaleX: 900.0 / b.width, y: 120 / b.height)
-            .scaledBy(x: 1, y: -1)
-            .translatedBy(x: 0, y: -150)
+
+        let t = CGAffineTransform(scaleX: 950.0 / b.width, y: 120 / b.height)
+            .translatedBy(x: 0, y: 30)
+
         return p.applying(t)
     }
 }
